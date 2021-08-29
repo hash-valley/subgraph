e0f56a27ff771785b54f14624917ef84db41fb24
Nate Mueller <inathan.m@gmail.com>
2021-08-29 13:30:36 -0700

    first iteration

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,102 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { Transfer as BottleTransfer, WineBottleV1 as BottleContract } from '../generated/WineBottleV1/WineBottleV1'
+import { Transfer as VineyardTransfer, VineyardMinted, Planted, Harvested, VineyardV1 as VineContract } from '../generated/VineyardV1/VineyardV1'
+import { Staked, Withdrawn, Spoiled } from '../generated/Cellar/CellarV1'
+import { Vineyard, Bottle, Account } from '../generated/schema'
+import { BigInt, Bytes, ipfs, json, log, Wrapped, JSONValue } from "@graphprotocol/graph-ts"
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+const CELLAR = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+
+function getOrCreateAccount(address: Bytes): Account {
+  let account = Account.load(address.toHex())
+  if (account == null) {
+    account = new Account(address.toHex())
+  }
+  account.save()
+  return account as Account
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+export function handleBottleTransfer(event: BottleTransfer): void {
+  let bottle = Bottle.load(event.params.tokenId.toHex())
+  if (bottle == null || event.params.from.toHex() == ZERO_ADDRESS) {
+    bottle = new Bottle(event.params.tokenId.toHex())
+    bottle.tokenId = event.params.tokenId
+    bottle.inCellar = false
+    bottle.spoiled = false
+    bottle.canEnterCellar = true
+    
+    let contract = BottleContract.bind(event.address)
+    bottle.attributes = contract.attributes(event.params.tokenId)
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+  if (event.params.to.toHex() != CELLAR) {
+    let account = getOrCreateAccount(event.params.to)
+    bottle.owner = account.id
+  }
+  bottle.save()
+}
+
+export function handleVineyardTransfer(event: VineyardTransfer): void {
+  let vineyard = Vineyard.load(event.params.tokenId.toHex())
+  if (vineyard == null || event.params.from.toHex() == ZERO_ADDRESS) {
+    vineyard = new Vineyard(event.params.tokenId.toHex())
+    vineyard.tokenId = event.params.tokenId
+    vineyard.location = 0
+    vineyard.elevation = 0
+    vineyard.soil = 0
+    vineyard.xp = BigInt.fromI32(0)
+    vineyard.seasonsPlanted = []
+    vineyard.seasonsHarvested = []
+  }
+  let account = getOrCreateAccount(event.params.to)
+  vineyard.owner = account.id
+  vineyard.save()
+}
+
+export function handleVineyardMinted(event: VineyardMinted): void {
+  let vineyard = Vineyard.load(event.params.tokenId.toHex())
+  vineyard.location = event.params.location.toI32()
+  vineyard.elevation = event.params.elevation.toI32()
+  if (event.params.elevationNegative.toI32() == 1) {
+    vineyard.elevation *= -1
+  }
+  vineyard.soil = event.params.soilType.toI32()
+  vineyard.save()
+}
+
+export function handlePlanted(event: Planted): void {
+  let vineyard = Vineyard.load(event.params.tokenId.toHex())
+  vineyard.seasonsPlanted = vineyard.seasonsPlanted.concat([event.params.season.toI32()])
+  vineyard.save()
+}
+
+export function handleHarvested(event: Harvested): void {
+  let vineyard = Vineyard.load(event.params.tokenId.toHex())
+  vineyard.seasonsHarvested = vineyard.seasonsHarvested.concat([event.params.season.toI32()])
+
+  let contract = VineContract.bind(event.address)
+  vineyard.xp = contract.xp(event.params.tokenId)
+  vineyard.save()
+
+  let bottle = Bottle.load(event.params.bottleId.toHex())
+  bottle.from = vineyard.id
+  bottle.save()
+}
+
+export function handleStaked(event: Staked): void {
+  let bottle = Bottle.load(event.params.tokenId.toHex())
+  bottle.inCellar = true
+  bottle.canEnterCellar = false
+  bottle.save()
+}
+
+export function handleWithdrawn(event: Withdrawn): void {
+  let bottle = Bottle.load(event.params.tokenId.toHex())
+  bottle.inCellar = false
+  bottle.save()
+}
+
+export function handleSpoiled(event: Spoiled): void {
+  let bottle = Bottle.load(event.params.tokenId.toHex())
+  bottle.spoiled = true
+  bottle.save()
 }
