46d3b34763d0d381919b453ce58d14c226178f02
Nate Mueller <inathan.m@gmail.com>
2022-03-20 19:04:08 +0000

    finish giveaway token intgration

--- src/giveaway.ts
+++ src/giveaway.ts
@@ -2,16 +2,16 @@ import { Transfer as GiveawayTransfer, Approval as GiveawayApproval } from "../g
 import { getOrCreateAccount, ZERO_ADDRESS } from "./utils";
 import { VineProtocol } from "../generated/schema";
 
-export function handleVinegarTransfer(event: GiveawayTransfer): void {
+export function handleGiveawayTransfer(event: GiveawayTransfer): void {
   if (event.params.from.toHex() != ZERO_ADDRESS) {
     let from = getOrCreateAccount(event.params.from);
-    from.vinegarBalance = from.vinegarBalance.minus(event.params.value);
+    from.giveawayBalance = from.giveawayBalance.minus(event.params.value);
     from.save();
   }
 
   if (event.params.to.toHex() != ZERO_ADDRESS) {
     let to = getOrCreateAccount(event.params.to);
-    to.vinegarBalance = to.vinegarBalance.plus(event.params.value);
+    to.giveawayBalance = to.giveawayBalance.plus(event.params.value);
     to.save();
   }
 }
