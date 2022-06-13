f03e69fedc71fd9dc376c56cd200475bcd60c799
Nate Mueller <inathan.m@gmail.com>
2022-06-13 08:49:48 -0400

    vinegar calc

--- src/cellar.ts
+++ src/cellar.ts
@@ -1,10 +1,12 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import {
   Staked,
   Withdrawn,
   Spoiled,
   Cellar as CellarContract,
 } from "../generated/Cellar/Cellar";
-import { Bottle } from "../generated/schema";
+import { Bottle, VineProtocol } from "../generated/schema";
+import { WineBottle as BottleContract } from "../generated/WineBottle/WineBottle";
 
 export function handleStaked(event: Staked): void {
   let bottle = Bottle.load(event.params.tokenId.toString()) as Bottle;
@@ -26,7 +28,18 @@ export function handleSpoiled(event: Spoiled): void {
   bottle.spoiled = true;
   bottle.inCellar = false;
 
-  let contract = CellarContract.bind(event.address);
-  bottle.rejuvenateCost = contract.cellarTime(event.params.tokenId);
+  let cellarTime = CellarContract.bind(event.address).cellarTime(
+    event.params.tokenId
+  );
+
+  let vineProtocol = VineProtocol.load("0") as VineProtocol;
+  let bottleAddress = Address.fromString(vineProtocol.bottle.toHex());
+  let bottleContract = BottleContract.bind(bottleAddress);
+  let cellarAged = bottleContract.cellarAged(cellarTime);
+
+  bottle.rejuvenateCost = cellarAged
+    .times(BigInt.fromString("1000000000000000000"))
+    .div(BigInt.fromI32(86400))
+    .times(BigInt.fromI32(3));
   bottle.save();
 }
